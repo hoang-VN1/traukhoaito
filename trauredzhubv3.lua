ee86c6e68e1946f91b81f01209e3acfd71277710
trauroblox <kienmatlol0@gmail.com>
2025-10-12 17:47:58 +0700

    Update trauredzhubv3.lua

--- trauredzhubv3.lua
+++ trauredzhubv3.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/da7efd7e2445fe43"))()
--- định làm gì đấy ?
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/37e8191f3a6faee5"))()
